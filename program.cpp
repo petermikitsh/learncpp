408f2725f96ff5c2d35e6b0bdd171a603f5f7b53
Peter Mikitsh <pmikitsh@zuora.com>
2021-12-30 14:29:29 -0800

    format code

--- program.cpp
+++ program.cpp
@@ -3,30 +3,32 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
 
-  int someHexValues[] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+  int someHexValues[] = {0x01, 0x02, 0x03, 0x04, 0x05};
   std::string result = "";
 
-  for(int i = 0; i < 5; i++) {
+  for (int i = 0; i < 5; i++)
+  {
     result.append(std::to_string(someHexValues[i]));
   }
 
   printf("Hello World. Result is: %s\n", result.c_str());
 
   // iterate area when length is unknown
-  int unknownArrayLength[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0xFFFF, INT_MAX };
+  int unknownArrayLength[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0xFFFF, INT_MAX};
   int len = *(&unknownArrayLength + 1) - unknownArrayLength;
 
   printf(
-    "1a: First: %d. Second: %d. First - second: %d.\n",
-    *(*(&unknownArrayLength + 1)),
-    *unknownArrayLength,
-    len
-  );
+      "1a: First: %d. Second: %d. First - second: %d.\n",
+      *(*(&unknownArrayLength + 1)),
+      *unknownArrayLength,
+      len);
 
   std::string result1a = "";
-  for(int i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++)
+  {
     std::string prefix = i == 0 ? "" : ",";
     result1a.append(prefix + std::to_string(unknownArrayLength[i]));
   }
