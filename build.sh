79a8af1c925ff84a1a97fa3ed8db25f2b59cae68
Peter Mikitsh <pmikitsh@zuora.com>
2021-12-30 13:15:26 -0800

    print a string

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+
+# Log Each command
+set -x
+
+# Bail on first error
+set -e
+
 g++ -o output program.cpp
--- program.cpp
+++ program.cpp
@@ -1,8 +1,17 @@
 // Your First C++ Program
 
 #include <iostream>
+#include <string>
 
 int main() {
-  std::cout << "Hello World!";
+
+  int someHexValues[] = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+  std::string result = "";
+
+  for(int i = 0; i < 5; i++) {
+    result.append(std::to_string(someHexValues[i]));
+  }
+
+  printf("Hello World. Result is: %s", result.c_str());
   return 0;
 }
